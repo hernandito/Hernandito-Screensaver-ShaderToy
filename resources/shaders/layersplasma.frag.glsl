48c22ed4304544e8f9d5317b172640de2019063c
hernandito <hernandoruiz001@gmail.com>
2025-03-18 11:56:24 -0400

    @

--- resources/shaders/layersplasma.frag.glsl
+++ resources/shaders/layersplasma.frag.glsl
@@ -1,63 +1,40 @@
-/*
-    "Cosmic" by @XorDev
-    
-    I love making these glowy shaders. This time I thought I'd try using discs instead.
-
-    Tweet: twitter.com/XorDev/status/1601060422819680256
-    Twigl: t.co/IhRk3HX4Kt
-    
-    <300 chars playlist: shadertoy.com/playlist/fXlGDN
+/* This animation is the material of my first youtube tutorial about creative 
+   coding, which is a video in which I try to introduce programmers to GLSL 
+   and to the wonderful world of shaders, while also trying to share my recent 
+   passion for this community.
+                                       Video URL: https://youtu.be/f4s1h2YETNY
 */
-void mainImage(out vec4 O, vec2 I)
-{
-    //Clear fragcolor (hacky)
-    O *= 0.;
-    //Initialize resolution for scaling
-    vec2 r=iResolution.xy,
-    //Save centered pixel coordinates
-    p = (I-r*.55)*mat2(1,-1,2,2);
-    
-    //Initialize loop iterator and arc angle
-    for(float i=0.,a;
-        //Loop 300 times
-        i++<3e1;
-        //Add with ring attenuation
-        O += .03 / (abs(length(I=p/(r+r-p).y)*8e1-i)+4e1/r.y)*
-        //Limit to arcs
-        clamp(cos(a=atan(I.y,I.x)*ceil(i*.1)+iTime*sin(i*i)+i*i),.0,.6)*
-        //Give them color
-        (cos(a-i+vec4(.2,.2,.3,0))+1.));
-        
+
+//https://iquilezles.org/articles/palettes/
+vec3 palette( float t ) {
+    vec3 a = vec3(0.1, 0.1, 0.1);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263,0.416,0.557);
+
+    return a + b*cos( 6.28318*(c*t+d) );
 }
 
-
-
-
-
-
-
-///Original [258]
-
-/*
-void mainImage(out vec4 O, vec2 I)
-{
-    //Clear fragcolor (hacky)
-    O *= 0.;
-    //Initialize resolution for scaling
-    vec2 r=iResolution.xy,
-    //Save centered pixel coordinates
-    p = I-r*.6;
+//https://www.shadertoy.com/view/mtyGWy
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+    vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
     
-    //Initialize loop iterator and arc angle
-    for(float i=0.,a;
-        //Loop 300 times
-        i++<3e1;
-        //Add with ring attenuation
-        O += 5e-3 / (abs(length(I=p*mat2(1,-1,2,2)/(r.y-p-p.yx))-i/4e1)+1./r.y)*
-        //Limit to arcs
-        clamp(cos(a=atan(I.y,I.x)*ceil(i*.1)+iTime*sin(i*i)+i*i),.0,.6)*
-        //Give them color
-        (cos(a-i+vec4(0,1,2,0))+1.) );
+    for (float i = 0.0; i < 4.0; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+
+        vec3 col = palette(length(uv0) + i*.4 + iTime*.4);
+
+        d = sin(d*38. + iTime)/8.;
+        d = abs(d);
+
+        d = pow(0.03 / d, 0.4);
+
+        finalColor += col * d;
+    }
         
+    fragColor = vec4(finalColor, 1.0);
 }
-*/
